6317ff3d9c803eb9b369876ebd911512e36325ee
Andy Li <you@example.com>
2024-11-08 17:26:27 -0500

    adapted a website template to run on node. fun times!

--- app/page.js
+++ app/page.js
@@ -1,101 +1,292 @@
-import Image from "next/image";
+import Head from "next/head";
+import "../public/assets/css/main.css";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <>
+      <Head>
+        <title>Spectral by HTML5 UP</title>
+        <meta charset="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=no"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <link rel="stylesheet" href="/assets/css/main.css" />
+        <noscript>
+          <link rel="stylesheet" href="/assets/css/noscript.css" />
+        </noscript>
+      </Head>
+      <div className="container">
+        {
+          <>
+            <div id="page-wrapper">
+              <header id="header" className="alt">
+                <h1>
+                  <a href="index.html">Spectral</a>
+                </h1>
+                <nav id="nav">
+                  <ul>
+                    <li>
+                      <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                      <a href="generic.html">Generic</a>
+                    </li>
+                    <li>
+                      <a href="elements.html">Elements</a>
+                    </li>
+                    <li>
+                      <a href="#">Sign Up</a>
+                    </li>
+                    <li>
+                      <a href="#">Log In</a>
+                    </li>
+                  </ul>
+                </nav>
+              </header>
+              <section id="banner">
+                <div className="inner">
+                  <h2>Spectral</h2>
+                  <p>
+                    Another fine responsive
+                    <br />
+                    site template freebie
+                    <br />
+                    crafted by <a href="http://html5up.net">HTML5 UP</a>.
+                  </p>
+                  <ul className="actions special">
+                    <li>
+                      <a href="#" className="button primary">
+                        Activate
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <a href="#one" className="more scrolly">
+                  Learn More
+                </a>
+              </section>
+              <section id="one" className="wrapper style1 special">
+                <div className="inner">
+                  <header className="major">
+                    <h2>
+                      Arcu aliquet vel lobortis ata nisl
+                      <br />
+                      eget augue amet aliquet nisl cep donec
+                    </h2>
+                    <p>
+                      Aliquam ut ex ut augue consectetur interdum. Donec amet
+                      imperdiet eleifend
+                      <br />
+                      fringilla tincidunt. Nullam dui leo Aenean mi ligula,
+                      rhoncus ullamcorper.
+                    </p>
+                  </header>
+                  <ul className="icons major">
+                    <li>
+                      <span className="icon fa-gem major style1">
+                        <span className="label">Lorem</span>
+                      </span>
+                    </li>
+                    <li>
+                      <span className="icon fa-heart major style2">
+                        <span className="label">Ipsum</span>
+                      </span>
+                    </li>
+                    <li>
+                      <span className="icon solid fa-code major style3">
+                        <span className="label">Dolor</span>
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              </section>
+              <section id="two" className="wrapper alt style2">
+                <section className="spotlight">
+                  <div className="image">
+                    <img src="images/pic01.jpg" alt="" />
+                  </div>
+                  <div className="content">
+                    <h2>
+                      Magna primis lobortis
+                      <br />
+                      sed ullamcorper
+                    </h2>
+                    <p>
+                      Aliquam ut ex ut augue consectetur interdum. Donec
+                      hendrerit imperdiet. Mauris eleifend fringilla nullam
+                      aenean mi ligula.
+                    </p>
+                  </div>
+                </section>
+                <section className="spotlight">
+                  <div className="image">
+                    <img src="images/pic02.jpg" alt="" />
+                  </div>
+                  <div className="content">
+                    <h2>
+                      Tortor dolore feugiat
+                      <br />
+                      elementum magna
+                    </h2>
+                    <p>
+                      Aliquam ut ex ut augue consectetur interdum. Donec
+                      hendrerit imperdiet. Mauris eleifend fringilla nullam
+                      aenean mi ligula.
+                    </p>
+                  </div>
+                </section>
+                <section className="spotlight">
+                  <div className="image">
+                    <img src="images/pic03.jpg" alt="" />
+                  </div>
+                  <div className="content">
+                    <h2>
+                      Augue eleifend aliquet
+                      <br />
+                      sed condimentum
+                    </h2>
+                    <p>
+                      Aliquam ut ex ut augue consectetur interdum. Donec
+                      hendrerit imperdiet. Mauris eleifend fringilla nullam
+                      aenean mi ligula.
+                    </p>
+                  </div>
+                </section>
+              </section>
+              <section id="three" className="wrapper style3 special">
+                <div className="inner">
+                  <header className="major">
+                    <h2>Accumsan mus tortor nunc aliquet</h2>
+                    <p>
+                      Aliquam ut ex ut augue consectetur interdum. Donec amet
+                      imperdiet eleifend
+                      <br />
+                      fringilla tincidunt. Nullam dui leo Aenean mi ligula,
+                      rhoncus ullamcorper.
+                    </p>
+                  </header>
+                  <ul className="features">
+                    <li className="icon fa-paper-plane">
+                      <h3>Arcu accumsan</h3>
+                      <p>
+                        Augue consectetur sed interdum imperdiet et ipsum.
+                        Mauris lorem tincidunt nullam amet leo Aenean ligula
+                        consequat consequat.
+                      </p>
+                    </li>
+                    <li className="icon solid fa-laptop">
+                      <h3>Ac Augue Eget</h3>
+                      <p>
+                        Augue consectetur sed interdum imperdiet et ipsum.
+                        Mauris lorem tincidunt nullam amet leo Aenean ligula
+                        consequat consequat.
+                      </p>
+                    </li>
+                    <li className="icon solid fa-code">
+                      <h3>Mus Scelerisque</h3>
+                      <p>
+                        Augue consectetur sed interdum imperdiet et ipsum.
+                        Mauris lorem tincidunt nullam amet leo Aenean ligula
+                        consequat consequat.
+                      </p>
+                    </li>
+                    <li className="icon solid fa-headphones-alt">
+                      <h3>Mauris Imperdiet</h3>
+                      <p>
+                        Augue consectetur sed interdum imperdiet et ipsum.
+                        Mauris lorem tincidunt nullam amet leo Aenean ligula
+                        consequat consequat.
+                      </p>
+                    </li>
+                    <li className="icon fa-heart">
+                      <h3>Aenean Primis</h3>
+                      <p>
+                        Augue consectetur sed interdum imperdiet et ipsum.
+                        Mauris lorem tincidunt nullam amet leo Aenean ligula
+                        consequat consequat.
+                      </p>
+                    </li>
+                    <li className="icon fa-flag">
+                      <h3>Tortor Ut</h3>
+                      <p>
+                        Augue consectetur sed interdum imperdiet et ipsum.
+                        Mauris lorem tincidunt nullam amet leo Aenean ligula
+                        consequat consequat.
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </section>
+              <section id="cta" className="wrapper style4">
+                <div className="inner">
+                  <header>
+                    <h2>Arcue ut vel commodo</h2>
+                    <p>
+                      Aliquam ut ex ut augue consectetur interdum endrerit
+                      imperdiet amet eleifend fringilla.
+                    </p>
+                  </header>
+                  <ul className="actions stacked">
+                    <li>
+                      <a href="#" className="button fit primary">
+                        Activate
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="button fit">
+                        Learn More
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </section>
+              <footer id="footer">
+                <ul className="icons">
+                  <li>
+                    <a href="#" className="icon brands fa-twitter">
+                      <span className="label">Twitter</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="icon brands fa-facebook-f">
+                      <span className="label">Facebook</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="icon brands fa-instagram">
+                      <span className="label">Instagram</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="icon brands fa-dribbble">
+                      <span className="label">Dribbble</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="icon solid fa-envelope">
+                      <span className="label">Email</span>
+                    </a>
+                  </li>
+                </ul>
+                <ul className="copyright">
+                  <li>&copy; Untitled</li>
+                  <li>
+                    Design: <a href="http://html5up.net">HTML5 UP</a>
+                  </li>
+                </ul>
+              </footer>
+            </div>
+            <script src="/assets/js/jquery.min.js"></script>
+            <script src="/assets/js/jquery.scrollex.min.js"></script>
+            <script src="/assets/js/jquery.scrolly.min.js"></script>
+            <script src="/assets/js/browser.min.js"></script>
+            <script src="/assets/js/breakpoints.min.js"></script>
+            <script src="/assets/js/util.js"></script>
+            <script src="/assets/js/main.js"></script>
+          </>
+        }
+      </div>
+    </>
   );
 }
